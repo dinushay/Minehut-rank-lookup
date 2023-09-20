5f110afae164ecec063a3c179b531320d286a00d
Dinu <61915816+DinuNuggets@users.noreply.github.com>
2023-09-20 07:19:03 +0200

    Correct Minecraft playernames

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ async def rank(interaction: nextcord.Interaction, playername: str):
     if mojang_response.status_code == 200:
         mojang_data = mojang_response.json()
         uuid = mojang_data["id"]
+        mcname = mojang_data["name"]
         uuid_with_dashes = f"{uuid[:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:20]}-{uuid[20:]}"
         minehut_url = f"https://api.minehut.com/cosmetics/profile/{uuid_with_dashes}"
         minehut_response = requests.get(minehut_url)
@@ -18,11 +19,11 @@ async def rank(interaction: nextcord.Interaction, playername: str):
             minehut_data = minehut_response.json()
             rank = minehut_data["rank"]
             if rank == "DEFAULT":
-                await interaction.response.send_message(f"`{playername}` has not subscribed to a Minehut rank.", ephemeral=False)
+                await interaction.response.send_message(f"`{mcname}` has not subscribed to a Minehut rank.", ephemeral=False)
             else:
-                await interaction.response.send_message(f"The rank of `{playername}` is `{rank}`.", ephemeral=False)
+                await interaction.response.send_message(f"The rank of `{mcname}` is `{rank}`.", ephemeral=False)
         else:
-            await interaction.response.send_message(f"`{playername}` has never joined Minehut.", ephemeral=False)
+            await interaction.response.send_message(f"`{ncname}` has never joined Minehut.", ephemeral=False)
     else:
         await interaction.response.send_message(f"This player `{playername}` does not exist.", ephemeral=False)
 
@@ -33,6 +34,7 @@ async def rank_private(interaction: nextcord.Interaction, playername: str):
     if mojang_response.status_code == 200:
         mojang_data = mojang_response.json()
         uuid = mojang_data["id"]
+        mcname = mojang_data["name"]
         uuid_with_dashes = f"{uuid[:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:20]}-{uuid[20:]}"
         minehut_url = f"https://api.minehut.com/cosmetics/profile/{uuid_with_dashes}"
         minehut_response = requests.get(minehut_url)
@@ -40,11 +42,11 @@ async def rank_private(interaction: nextcord.Interaction, playername: str):
             minehut_data = minehut_response.json()
             rank = minehut_data["rank"]
             if rank == "DEFAULT":
-                await interaction.response.send_message(f"`{playername}` has not subscribed to a Minehut rank.", ephemeral=True)
+                await interaction.response.send_message(f"`{mcname}` has not subscribed to a Minehut rank.", ephemeral=True)
             else:
-                await interaction.response.send_message(f"The rank of `{playername}` is `{rank}`.", ephemeral=True)
+                await interaction.response.send_message(f"The rank of `{mcname}` is `{rank}`.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"`{playername}` has never joined Minehut.", ephemeral=True)
+            await interaction.response.send_message(f"`{mcname}` has never joined Minehut.", ephemeral=True)
     else:
         await interaction.response.send_message(f"This player `{playername}` does not exist.", ephemeral=True)
 
